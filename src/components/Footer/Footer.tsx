f7bf1f50446a6b3f2c4ccbcb07838a4ae84036eb
Gabriel Lehmann <gabriel.lehmann@hotmail.com>
2024-05-09 14:50:13 -0300

    feat: footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Container } from "./styled";
+
+export default function Footer() {
+  return (
+    <Container>
+      <p>MKS sistemas © Todos os direitos reservados</p>
+    </Container>
+  );
+}
---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+  width: 100%;
+  height: 34px;
+  background: #eeeeee;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
